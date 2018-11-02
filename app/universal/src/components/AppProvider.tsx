e6504c05b9c1e0e538c9f6724dc7b9f6139b0652
OKUNOKENTARO <okunokentaro@me.com>
2018-11-02 11:28:11 +0900

    [Progress] implement similar to combineReducers

--- app/universal/src/components/AppProvider.tsx
+++ app/universal/src/components/AppProvider.tsx
@@ -1,8 +1,16 @@
-import * as React from 'react'
+import * as React from 'react';
 
-const initialState = {todos: []}
+const initialState = { todos: [] };
+const initialSettingsState = { settings: [] };
 
 export const TodosContext = React.createContext({ state: initialState, dispatch: undefined } as {
+  state: any;
+  dispatch: any;
+});
+export const SettingsContext = React.createContext({
+  state: initialSettingsState,
+  dispatch: undefined,
+} as {
   state: any;
   dispatch: any;
 });
@@ -10,20 +18,46 @@ export const TodosContext = React.createContext({ state: initialState, dispatch:
 const todosReducer = (state: any, action: { type: string; payload: any }) => {
   switch (action.type) {
     case 'add':
-      console.log('add')
-      const newState = Object.assign(state, {todos: state.todos.concat([action.payload.text])})
-      console.log(newState)
-      return newState
+      console.log('todosReducer add');
+      const newState = Object.assign(state, { todos: state.todos.concat([action.payload.text]) });
+      console.log(newState);
+      return newState;
     default:
-      return state
+      return state;
   }
-}
+};
+
+const settingsReducer = (state: any, action: { type: string; payload: any }) => {
+  switch (action.type) {
+    case 'add':
+      console.log('settingsReducer add');
+      const newState = Object.assign(state, {
+        settings: state.settings.concat([action.payload.text]),
+      });
+      console.log(newState);
+      return newState;
+    default:
+      return state;
+  }
+};
 
 export function AppProvider({ children }: any) {
   const [todosState, todosDispatch] = (React as any).useReducer(todosReducer, initialState);
+  const [settingsState, settingsDispatch] = (React as any).useReducer(
+    settingsReducer,
+    initialSettingsState,
+  );
+
+  const dispatch = (v: any) => {
+    todosDispatch(v);
+    settingsDispatch(v);
+  }
+
   return (
-    <TodosContext.Provider value={{ state: todosState, dispatch: todosDispatch }}>
-      {children}
+    <TodosContext.Provider value={{ state: todosState, dispatch }}>
+      <SettingsContext.Provider value={{ state: settingsState, dispatch }}>
+        {children}
+      </SettingsContext.Provider>
     </TodosContext.Provider>
   );
 }
